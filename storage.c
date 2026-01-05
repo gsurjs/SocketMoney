7a9195d80e8510336df059d423f670bf702add94
gsurjs <155509989+gsurjs@users.noreply.github.com>
2026-01-05 13:51:23 -0500

    adding view expenses and view total functions for db logic

--- storage.c
+++ storage.c
@@ -91,3 +91,45 @@ void add_expense(sqlite3 *db) {
     }
     sqlite3_finalize(stmt); // free resources allocated to the statement
 }
+
+void view_expenses(sqlite3 *db) {
+   sqlite3_stmt *stmt;
+   char *sql = "SELECT id, date, category, amount, description FROM expenses ORDER BY date DESC;";
+   // prepare query
+   if (sqlite3_prepare_v2(db, sql, -1, &stmt, 0) != SQLITE_OK) {
+       fprintf(stderr, "ERROR: Failed to prepare statement: %s\n", sqlite3_errmsg(db));
+       return;
+   }
+
+   // formatting left-align string within 5 spaces
+   printf("\n%-5s %-12s %-15s %-10s %-20s\n", "ID", "Date", "Category", "Amount", "Description");
+   printf("---------------------------------------------------------------\n");
+
+   // loop through results
+   // sqlite3_step returns SQLITE+ROW if there is data to read
+   while (sqlite3_step(stmt) == SQLITE_ROW) {
+       int id = sqlite3_column_int(stmt, 0);
+       const unsigned char *date = sqlite3_column_text(stmt, 1);
+       const unsigned char *category = sqlite3_column_text(stmt, 2);
+       double amount = sqlite3_column_double(stmt, 3);
+       const unsigned char *description = sqlite3_column_text(stmt, 4);
+
+       printf("%-5d %-12s %-15s $%-9.2f %-20s\n", id, date, category, amount, description);
+   }
+
+    sqlite3_finalize(stmt); // free resources
+
+}
+
+void view_total(sqlite3 *db) {
+    sqlite3_stmt *stmt;
+    // aggregate fnction for letting sql engine to the sum
+    char *sql = "SELECT SUM(amount) FROM expenses;";
+    if (sqlite3_prepare_v2(db, sql, -1, &stmt, 0) != SQLITE_OK) {
+        if (sqlite3_step(stmt) == SQLITE_ROW) {
+            double total = sqlite3_column_double(stmt, 0);
+            printf("\n>> Total Lifetime Spending: $%.2f\n", total);
+        }
+    }
+    sqlite3_finalize(stmt); // free resources
+}
